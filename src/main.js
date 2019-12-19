d9ae6b555ae4eba405701a8fb788312fc790de53
yangshuai <286319700@qq.com>
2019-12-19 19:48:00 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import router from './router'
 import './styles/index.less'// 初始化样式
 import ElementUI from 'element-ui'// 引入UI
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
-
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.prototype.$http = axios// 赋值给全局对象
 Vue.config.productionTip = false
+
 Vue.use(ElementUI)
+
 new Vue({
   router,
   render: h => h(App)
