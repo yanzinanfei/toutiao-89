be29f640d62b9db6236ee5dd570065b79097a9d6
yangshuai <286319700@qq.com>
2019-12-20 19:29:25 +0800

    -主页模块-默认导航

---
+++ src/views/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <!-- 二级组件的默认组件 -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+.home {
+  background-image: url('../../assets/img/背景图.jpg');
+  height: 100vh;
+  background-size: cover;
+}
+</style>
