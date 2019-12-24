05962553aaa39623b8f4f3e35b4731efb0892068
yangshuai <286319700@qq.com>
2019-12-24 16:34:11 +0800

    PC-素材列表-新建页面-挂载路由

--- src/router/index.js
+++ src/router/index.js
@@ -20,8 +20,11 @@ const routes = [
       component: Home2
     }, {
       // 二级路由表
-      path: '/home/comment', // 完整地址 相对地址
+      path: 'comment', // 完整地址 相对地址
       component: () => import('../views/comment') // 按需加载
+    }, {
+      path: 'material',
+      component: () => import('../views/material')// 按需加载 素材列表
     }]
   }, {
   // 登录页
