aad9559d36efadaf761da53cb765b15e85eed1bb
yangshuai <286319700@qq.com>
2019-12-29 10:23:03 +0800

    PC-账户信息-页面结构

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -3,15 +3,42 @@
     <bread-crumb slot="header">
       <template slot="title">账户信息</template>
     </bread-crumb>
+    <el-upload class="head-upload" action="" :show-file-list="false">
+      <img src="../../assets/img/2.jpg" alt="">
+    </el-upload>
+    <el-form style="margin-left:50px" label-width="80px">
+      <el-form-item label="用户名">
+        <el-input style="width:30%"></el-input>
+      </el-form-item>
+      <el-form-item label="简介">
+        <el-input style="width:30%"></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱">
+        <el-input style="width:30%"></el-input>
+      </el-form-item>
+      <el-form-item label="手机号">
+        <el-input disabled style="width:30%"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">保存信息</el-button>
+      </el-form-item>
+    </el-form>
   </el-card>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang='less' scoped>
+  .head-upload {
+    position: absolute;
+    right: 300px;
+    top: 150px;
+    img {
+      width: 200px;
+      height: 200px;
+      border-radius: 50%;
+    }
+  }
 </style>
