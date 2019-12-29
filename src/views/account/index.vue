c2c42ff16def7e3b1fbc9bdb8bde62500dbd0437
yangshuai <286319700@qq.com>
2019-12-29 14:38:50 +0800

    PC-账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -3,21 +3,21 @@
     <bread-crumb slot="header">
       <template slot="title">账户信息</template>
     </bread-crumb>
-    <el-upload class="head-upload" action="" :show-file-list="false">
-      <img src="../../assets/img/2.jpg" alt="">
+    <el-upload class="head-upload" action :show-file-list="false">
+      <img :src="formData.photo ? formData.photo : defaultImg" alt />
     </el-upload>
     <el-form style="margin-left:50px" label-width="80px">
       <el-form-item label="用户名">
-        <el-input style="width:30%"></el-input>
+        <el-input v-model="formData.name" style="width:30%"></el-input>
       </el-form-item>
       <el-form-item label="简介">
-        <el-input style="width:30%"></el-input>
+        <el-input v-model="formData.intro" style="width:30%"></el-input>
       </el-form-item>
       <el-form-item label="邮箱">
-        <el-input style="width:30%"></el-input>
+        <el-input v-model="formData.email" style="width:30%"></el-input>
       </el-form-item>
       <el-form-item label="手机号">
-        <el-input disabled style="width:30%"></el-input>
+        <el-input v-model="formData.mobile" disabled style="width:30%"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary">保存信息</el-button>
@@ -27,18 +27,45 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      // 定义一个表单数据对象
+      formData: {
+        name: '', // 用户名
+        intro: '', // 简介
+        photo: '', // 头像
+        email: '', // 邮箱
+        mobile: '' // 手机号
+      },
+      defaultImg: require('../../assets/img/2.jpg')
+    }
+  },
+  methods: {
+    // 获取用户信息
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        this.formData = result.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo() // 用户个人信息
+  }
+}
 </script>
 
 <style lang='less' scoped>
-  .head-upload {
-    position: absolute;
-    right: 300px;
-    top: 150px;
-    img {
-      width: 200px;
-      height: 200px;
-      border-radius: 50%;
-    }
+.head-upload {
+  position: absolute;
+  right: 300px;
+  top: 150px;
+  img {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
   }
+}
 </style>
