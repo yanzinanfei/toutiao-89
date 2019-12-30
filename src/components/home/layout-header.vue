02cb5ba56cbc2d77cd784fda791fc1a5d5f4b8ce
yangshuai <286319700@qq.com>
2019-12-30 16:56:57 +0800

    PC-账户信息更新-同步到头部页面之eventBus

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -1,79 +1,104 @@
 <template>
   <!-- 头部组件 -->
-  <el-row class="layout-header" type="flex" align="middle" justify="space-between" >
+  <el-row class='layout-header' type="flex" align="middle" justify="space-between">
       <!-- 左侧 -->
-    <el-col class="left" :span="6">
-      <!-- 左侧图标 -->
-      <i class="el-icon-s-fold"></i>
-      <span class="title">江苏传智播客教育科技股份有限公司</span>
-    </el-col>
+      <el-col class='left' :span="6">
+          <!-- 左侧图标 -->
+          <i @click="collaspseOrOpen" :class="{'el-icon-s-unfold': collaspse,'el-icon-s-fold': !collaspse}"></i>
+          <span class='title'>江苏传智播客教育科技股份有限公司</span>
+      </el-col>
       <!-- 右侧 -->
-    <el-col class="right" :span="4">
-      <el-row type='flex' justify="end" align="middle">
-        <img :src="!userInfo.photo ? userInfo.photo : defaultImg" alt="">
-        <!-- 下拉菜单 -->
-        <el-dropdown @command="handle">
-          <span>{{ userInfo.name }}</span>
-          <el-dropdown-menu slot="dropdown">
-            <!-- 具名插槽 -->
-            <el-dropdown-item command="info">个人信息</el-dropdown-item>
-            <el-dropdown-item command="git">Git地址</el-dropdown-item>
-            <el-dropdown-item command="lgout">退出</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-      </el-row>
-    </el-col>
+      <el-col class='right' :span="4">
+          <el-row type='flex' justify="end" align="middle">
+            <img :src="userInfo.photo ? userInfo.photo : defaultImg" alt="">
+             <!-- 下拉菜单 -->
+             <el-dropdown  @command="handle">
+                 <span>{{ userInfo.name }}</span>
+                 <!-- 下拉菜单  具名插槽 -->
+                 <el-dropdown-menu slot="dropdown">
+                     <!-- 下拉内容 -->
+                      <el-dropdown-item command="info">个人信息</el-dropdown-item>
+                      <el-dropdown-item command="git">Git地址</el-dropdown-item>
+                      <el-dropdown-item command="lgout">退出</el-dropdown-item>
+                 </el-dropdown-menu>
+            </el-dropdown>
+          </el-row>
+      </el-col>
+
   </el-row>
 </template>
 
 <script>
+import eventBus from '../../utils/eventBus' // 引入公共实例
 export default {
   data () {
     return {
+      collaspse: false, // 是否折叠
       userInfo: {}, // 用户信息
-      defaultImg: require('../../assets/img/2.jpg') // 先把地址换成字符串
+      defaultImg: require('../../assets/img/2.jpg') // 先把地址转换成变量
     }
   },
-  created () { // 查询数据
-    this.$axios({
-      url: '/user/profile'
-      // headers参数
-    }).then(result => {
-      this.userInfo = result.data // 获取用户个人信息
+  created () {
+    // 查询数据
+    this.getUserInfo()
+    // 实例创建完毕 立刻监听
+    eventBus.$on('updateUserInfoSuccess', () => {
+      // 别人告诉你 它更新了数据 应该立刻处理
+      this.getUserInfo()
     })
   },
   methods: {
-    handle (command) {
+    collaspseOrOpen () {
+      // 直接取反
+      this.collaspse = !this.collaspse // 直接返回
+      // 通知要改变宽度的组件
+      eventBus.$emit('changeCollapse') // 改变了折叠状态
+    },
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      //   headers参数
+      }).then(result => {
+        this.userInfo = result.data // 获取用户个人信息
+      })
+    },
+    handle (commad) {
       // 区分点击的菜单项
-      if (command === 'lgout') {
+      if (commad === 'lgout') {
+        //   退出
         window.localStorage.removeItem('user-token') // 删除用户的令牌
         this.$router.push('/login')
-      } else if (command === 'git') {
-        window.location.href = 'https://github.com/yanzinanfei/toutiao-89'
+      } else if (commad === 'git') {
+        window.location.href = 'https://github.com/yangshuai/toutiao-89'
+      } else if (commad === 'info') {
+        this.$router.push('/home/account') // 跳转到账户信息页面
       }
     }
   }
 }
 </script>
 
-<style lang="less" scoped>
+<style lang='less' scoped>
   .layout-header {
-    height: 60px;
-    .left {
-      font-size: 18px;
-      .title {
-        margin-left: 4px;
-        color: #2c3e50;
-        font-size: 14px;
+      height:60px;
+      .left {
+          font-size: 18px;
+          .title {
+              margin-left:4px;
+              color: #2c3e50;
+              font-size:16px;
+          }
+          i {
+            font-size:22px;
+          }
       }
-    }
-    .right {
-      img {
-        width: 40px;
-        height: 40px;
-        border-radius: 50%;
-        margin-right: 5px;
+      .right {
+          img {
+              width:40px;
+              height: 40px;
+              border-radius: 50%;
+              margin-right:5px;
+          }
       }
-    }
   }
 </style>
--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,45 +1,40 @@
 <template>
-  <el-card v-loading='loading'>
-    <bread-crumb slot="header">
-      <template slot="title">账户信息</template>
-    </bread-crumb>
-    <!-- 放置上传组件 -->
-    <el-upload :http-request="uploadImg" class="head-upload" action :show-file-list="false">
-      <img :src="formData.photo ? formData.photo : defaultImg" alt />
-    </el-upload>
-    <!-- 放置组件 -->
-    <el-form
-      ref="myForm"
-      :model="formData"
-      :rules="nameRules"
-      style="margin-left:50px"
-      label-width="80px"
-    >
-      <el-form-item label="用户名" prop="name">
-        <el-input v-model="formData.name" style="width:30%"></el-input>
-      </el-form-item>
-      <el-form-item label="简介" prop="intro">
-        <el-input v-model="formData.intro" style="width:30%"></el-input>
-      </el-form-item>
-      <el-form-item label="邮箱" prop="email">
-        <el-input v-model="formData.email" style="width:30%"></el-input>
-      </el-form-item>
-      <el-form-item label="手机号">
-        <el-input v-model="formData.mobile" disabled style="width:30%"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button @click="saveUserInfo" type="primary">保存信息</el-button>
-      </el-form-item>
-    </el-form>
+  <el-card v-loading="loading">
+      <bread-crumb slot='header'>
+         <template slot='title'>账户信息</template>
+      </bread-crumb>
+      <!-- 放置上传组件 -->
+      <el-upload  :http-request="uploadImg" class='head-upload' action="" :show-file-list="false">
+          <img :src="formData.photo ? formData.photo : defaultImg" alt="">
+      </el-upload>
+      <!-- 放置组件 -->
+     <el-form ref="myForm" :model="formData" :rules="rules" style="margin-left:100px" label-width="100px">
+         <el-form-item label="用户名" prop="name">
+             <el-input v-model="formData.name" style="width:30%"></el-input>
+         </el-form-item>
+         <el-form-item label="简介" prop="intro">
+              <el-input v-model="formData.intro"  style="width:30%"></el-input>
+         </el-form-item>
+         <el-form-item label="邮箱" prop="email">
+              <el-input v-model="formData.email"  style="width:30%"></el-input>
+         </el-form-item>
+         <el-form-item  label="手机号">
+              <el-input v-model="formData.mobile" disabled style="width:30%"></el-input>
+             </el-form-item>
+             <el-form-item>
+                 <el-button @click="saveUserInfo" type="primary">保存信息</el-button>
+            </el-form-item>
+     </el-form>
   </el-card>
 </template>
 
 <script>
+import eventBus from '../../utils/eventBus' // 引入了公共的实例
 export default {
   data () {
     return {
-      loading: false,
       // 定义一个表单数据对象
+      loading: false,
       formData: {
         name: '', // 用户名
         intro: '', // 简介
@@ -47,42 +42,39 @@ export default {
         email: '', // 邮箱
         mobile: '' // 手机号
       },
-      nameRules: {
-        name: [
-          { required: true, message: '用户名不能为空' },
+      rules: {
+        name: [{ required: true, message: '用户名不能为空' }, {
+          min: 1,
+          max: 7,
+          message: '用户名的长度限制为1到7个字符'
+        }],
+        email: [{ required: true, message: '邮箱不能为空' },
           {
-            min: 1,
-            max: 7,
-            message: '用户名的长度限制为1到7个字符'
-          }
-        ],
-        email: [
-          { required: true, message: '邮箱不能为空' },
-          {
-            pattern: /^([a-zA-Z0-9]+[_|_|.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|_|.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/,
+            pattern: /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/,
             message: '邮箱格式不正确'
-          }
-        ]
+          }]
       }, // 定义一个规则
       defaultImg: require('../../assets/img/2.jpg')
     }
   },
   methods: {
-    // 上传图片
+    //   上传图片
     uploadImg (params) {
       this.loading = true // 打开弹层
-      let data = new FormData() // 实例化一个对象
+      let data = new FormData() // 实例化对象
       data.append('photo', params.file) // 加入参数
       this.$axios({
-        url: 'user/photo',
+        url: '/user/photo',
         method: 'patch',
         data
       }).then(result => {
-        this.formData.photo = result.data.photo // 设置头像
-        this.loading = false // 关闭弹层
+        this.formData.photo = result.data.photo // 设置头像地址
+        eventBus.$emit('updateUserInfoSuccess') // 触发一个自定义事件 updateUserInfoSuccess
+
+        this.loading = false // 关调弹层
       })
     },
-    // 获取用户信息
+    //   获取用户信息
     getUserInfo () {
       this.$axios({
         url: '/user/profile'
@@ -90,22 +82,26 @@ export default {
         this.formData = result.data
       })
     },
+    // baocunyonghuxin
+    // sucaiGuanli
     saveUserInfo () {
-      // 保存用户信息
-      // 校验表单数据是否OK
-      this.$refs.myForm.validate(isOK => {
-        // 保存方法
+      // 要去校验表单数据是否OK
+      this.$refs.myForm.validate((isOK) => {
+        //   调用保存方法
         if (isOK) {
           this.$axios({
             url: '/user/profile',
             method: 'patch',
             data: this.formData
           }).then(result => {
-            // 认为保存成功
+            //   认为保存成功
             this.$message({
               type: 'success',
-              message: '保存用户信息成功'
+              message: '保存信息成功'
             })
+            // 需要告诉头部组件 我更新成功了 你也要更新
+            // this.$emit('')
+            eventBus.$emit('updateUserInfoSuccess') // 触发一个自定义事件 updateUserInfoSuccess
           })
         }
       })
@@ -118,14 +114,13 @@ export default {
 </script>
 
 <style lang='less' scoped>
-.head-upload {
-  position: absolute;
-  right: 300px;
-  top: 150px;
-  img {
-    width: 200px;
-    height: 200px;
-    border-radius: 50%;
-  }
-}
+ .head-upload {
+     position: absolute;
+     right: 300px;
+     img {
+         width: 200px;
+         height: 200px;
+         border-radius: 50%;
+     }
+ }
 </style>
